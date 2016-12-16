63a7bcba6043cda35a88932946cb5bfadf6439b1
Aurghyadip Kundu <adkundu@gmail.com>
2016-12-16 12:30:43 +0530

    Updated Phyllotaxis

--- Phyllotaxis/sketch.js
+++ Phyllotaxis/sketch.js
@@ -1,24 +1,34 @@
 var  n = 0;
 var c = 4;
 
+var start = 0;
+
 function setup() {
-  createCanvas(800,600);
+  createCanvas(400,400);
   background(0);
   angleMode(DEGREES);
-  colorMode(HSB);
+  colorMode(HSB,360,255,255);
 }
 
 function draw() {
+  //background(0);
+  translate(width/2,height/2);
+  rotate(n*0.3);
 
-  var a = n * 137.5;
-  var r = c * sqrt(n);
+  for (var i = 0; i < n; i++) {
+    var a = i * 137.5;
+    var r = c * sqrt(n);
 
-  var x = r * cos(a) + width/2;
-  var y = r * sin(a) + height/2;
+    var x = r * cos(a);// + width/2;
+    var y = r * sin(a);// + height/2;
+    hu = i/3.0 % 360;
 
-  fill((a - r) % 255, 255, 255);
-  noStroke();
-  ellipse(x,y, 4, 4);
+    fill(hu, 255, 255);
+    noStroke();
+    ellipse(x,y, 4, 4);
 
-  n++;
+  }
+
+  n += 5;
+  start += 5;
 }
